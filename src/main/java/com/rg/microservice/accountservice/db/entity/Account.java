9e79d13e216f106778281fb13437d96447cf38da
mas rosyid <rosyidgrobogan@gmail.com>
2020-09-05 10:15:37 +0700

    entity: Account

---
+++ src/main/java/com/rg/microservice/accountservice/db/entity/Account.java
@@ -0,0 +1,23 @@
+package com.rg.microservice.accountservice.db.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue
+    private  Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private  String password;
+}
