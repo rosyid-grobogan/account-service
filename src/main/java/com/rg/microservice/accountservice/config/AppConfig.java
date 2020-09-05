fef491eba617977048f0664f55da2175b7ea171a
mas rosyid <rosyidgrobogan@gmail.com>
2020-09-05 09:37:08 +0700

    config: AppConfig

---
+++ src/main/java/com/rg/microservice/accountservice/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.rg.microservice.accountservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AppConfig {
+}
