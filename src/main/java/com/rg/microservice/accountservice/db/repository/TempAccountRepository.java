d0acc3d1c639a1c6511b828db3d386a3e750dc8a
mas rosyid <rosyidgrobogan@gmail.com>
2020-09-07 18:28:27 +0700

    fixing

--- src/main/java/com/rg/microservice/accountservice/db/repository/TempAccountRepository.java
+++ src/main/java/com/rg/microservice/accountservice/db/repository/TempAccountRepository.java
@@ -1,9 +1,9 @@
 package com.rg.microservice.accountservice.db.repository;
 
 import com.rg.microservice.accountservice.db.entity.TempAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TempAccountRepository extends JpaRepository<TempAccount, Long> {
+public interface TempAccountRepository extends CrudRepository<TempAccount, String>{
 }
